69f74950718255295e2e2808de4564df94043181
Trevor Behlman <tsbehlman@gmail.com>
2017-01-25 00:16:31 -0500

    Accept buffer as input

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 #slim-transformer
 
-Small, fast module for arbitrarily inserting text into a larger text.
+Small, fast module for arbitrarily inserting text into a larger text buffer.
--- spec/MainSpec.js
+++ spec/MainSpec.js
@@ -2,23 +2,23 @@ let Transformer = require( "../index" );
 
 describe( "Transformer", () => {
 	it( "initializes", () => {
-		let transformer = new Transformer( "test source" );
+		let transformer = new Transformer( Buffer.from( "test source" ) );
 	} );
 	
 	it( "emits no code", () => {
-		let transformer = new Transformer( "" );
+		let transformer = new Transformer( Buffer.from( "" ) );
 		
 		expect( transformer.getSource().toString() ).toBe( "" );
 	} );
 	
 	it( "emits original content", () => {
-		let transformer = new Transformer( "original" );
+		let transformer = new Transformer( Buffer.from( "original" ) );
 		
 		expect( transformer.getSource().toString() ).toBe( "original" );
 	} );
 	
 	it( "emits code at specified location", () => {
-		let transformer = new Transformer( "original" );
+		let transformer = new Transformer( Buffer.from( "original" ) );
 		
 		transformer.writeAt( "<>", 4 );
 		
@@ -26,7 +26,7 @@ describe( "Transformer", () => {
 	} );
 	
 	it( "emits code at two specified locations in order", () => {
-		let transformer = new Transformer( "original" );
+		let transformer = new Transformer( Buffer.from( "original" ) );
 		
 		transformer.writeAt( "<>", 0 );
 		transformer.writeAt( "><", 8 );
@@ -35,7 +35,7 @@ describe( "Transformer", () => {
 	} );
 	
 	it( "emits code at two specified locations out of order", () => {
-		let transformer = new Transformer( "original" );
+		let transformer = new Transformer( Buffer.from( "original" ) );
 		
 		transformer.writeAt( "<>", 6 );
 		transformer.writeAt( "><", 4 );
